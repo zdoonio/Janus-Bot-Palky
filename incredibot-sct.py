4dd2415f486c8abc55704c8f96586ff4e0af8771
Zdoonio <dominik.zdunczyk@gmail.com>
2022-05-04 20:03:50 +0200

    change flag to realtime

--- incredibot-sct.py
+++ incredibot-sct.py
@@ -594,7 +594,7 @@ result = run_game(  # run_game is a function that runs the game.
     maps.get("2000AtmospheresAIE"), # the map we are playing on
     [Bot(Race.Protoss, IncrediBot()), # runs our coded bot, protoss race, and we pass our bot object 
      Computer(Race.Zerg, Difficulty.Hard)], # runs a pre-made computer agent, zerg race, with a hard difficulty.
-    realtime=False, # When set to True, the agent is limited in how long each step can take to process.
+    realtime=True, # When set to True, the agent is limited in how long each step can take to process.
 )
 
 
