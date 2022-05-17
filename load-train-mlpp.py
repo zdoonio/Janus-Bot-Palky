eda9c5abe9b842c1a8c6298f47a17a94d1002b2f
Zdoonio <dominik.zdunczyk@gmail.com>
2022-05-17 10:47:22 +0200

    version

--- load-train-mlpp.py
+++ load-train-mlpp.py
@@ -11,7 +11,7 @@ from stable_baselines3.common.sb2_compat.rmsprop_tf_like import RMSpropTFLike
 import wandb
 
 
-LOAD_MODEL = "data/models/janusmind/v0_3_0"
+LOAD_MODEL = "data/models/janusmind/v0_3_1"
 # Environment:
 env = Sc2Env(is_train = True)
 
