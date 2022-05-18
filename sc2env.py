00854a982b413631043b7b719e6b41e37d870a95
Zdoonio <dominik.zdunczyk@gmail.com>
2022-05-19 00:35:39 +0200

    discrete space changed

--- sc2env.py
+++ sc2env.py
@@ -13,7 +13,7 @@ class Sc2Env(gym.Env):
 		# Define action and observation space
 		# They must be gym.spaces objects
 		# Example when using discrete actions:
-		self.action_space = spaces.Discrete(24)
+		self.action_space = spaces.Discrete(22)
 		self.observation_space = spaces.Box(low=0, high=255,
 											shape=(160, 160, 3), dtype=np.uint8)
 		self.is_train = is_train
